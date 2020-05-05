2d0fbce6d2457af1621c62b51a26f8c625b699b3
Pierce T <ptrotman22@gmail.com>
2020-05-05 15:17:40 -0400

    Set up for Hibernate completed
    This is the untested version.

---
+++ backend/src/main/java/com/example/demo/dao/EmployeeDaoImpl.java
@@ -0,0 +1,58 @@
+package com.example.demo.dao;
+
+//IMPORTANT If your code is not working your imports might be incorrect
+import com.example.demo.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+public class EmployeeDaoImpl implements EmployeeDAO {
+
+    //Define field for entity manager
+    private EntityManager entityManager;
+
+    //Set up constructor injection
+    @Autowired
+    public EmployeeDaoImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional //Defines the scope of a single database transaction.
+    public List<Employee> findAll() {
+        Session currentSession = entityManager.unwrap(Session.class);
+        Query<Employee> myQuery = currentSession.createQuery("from UserEmployee");
+        List<Employee> employees = myQuery.getResultList();
+        return employees;
+    }
+
+    @Override
+    @Transactional //Defines the scope of a single database transaction.
+    public Employee findById(int theId) {
+        Session currentSession = entityManager.unwrap(Session.class);
+        Employee theEmployee = currentSession.get(Employee.class, theId);
+        return theEmployee;
+    }
+
+    @Override
+    @Transactional //Defines the scope of a single database transaction.
+    public void save(Employee theEmployee) {
+        Session currentSession = entityManager.unwrap(Session.class);
+        currentSession.saveOrUpdate(theEmployee);
+
+    }
+
+    @Override
+    @Transactional //Defines the scope of a single database transaction.
+    public void deleteById(int theId) {
+        Session currentSession = entityManager.unwrap(Session.class);
+        Query<Employee> theQuery = currentSession.createQuery("delete from UserEmployee where id=:ID");
+        theQuery.setParameter("ID", theId);
+        theQuery.executeUpdate();
+    }
+}
