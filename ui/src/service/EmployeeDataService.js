968c25ab4c2792cd6cae194e4146196e0f26db5f
Aarth Thakore <aarth.thakore@gmail.com>
2020-05-14 21:57:08 -0400

    added delete functionality to adminconsole

--- ui/src/service/EmployeeDataService.js
+++ ui/src/service/EmployeeDataService.js
@@ -1,7 +1,26 @@
 import axios from 'axios'
 
 class EmployeeDataService {
+
   retrieveAllEmployees() {
     return axios.get(`http://localhost:8080/retrieveAllEmployees`)
   }
+
+  retrievePasswordByEmail(email) {
+    return axios.get(`http://localhost:8080/email/${email}`)
+  }
+
+  addEmployee() {
+    return axios.get(`http://localhost:8080/addEmployee`)
+  }
+
+  updateEmployee() {
+    return axios.get(`http://localhost:8080/updateEmployee`)
+  }
+
+  deleteEmployee(id) {
+    return axios.get(`http://localhost:8080/deleteEmployee/${id}`)
+  }
 }
+
+export default new EmployeeDataService()
